0d2776aa2641efa25319df65048973d196998f9a
Nikesh <Nikesh.diyali.2021@student.lhschools.org>
2020-03-02 13:46:20 -0500

    IntelliJ commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is");
+        System.out.println("Nikesh Diyali");
+
+        System.out.println("I spend my summer playing soccer mostly");
+        System.out.println("I went to Colorado last summer");
+        System.out.println("I played a lot of video games over the last summer");
+
+
+    }
+}
